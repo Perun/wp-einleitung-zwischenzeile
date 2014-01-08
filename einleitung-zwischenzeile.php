c1ee7aa2d3af85bb9a8a4b3b9e1d01ee502ec331
Vladimir Simovic <info@perun.net>
2014-01-08 01:45:23 +0100

    Update einleitung-zwischenzeile.php

--- einleitung-zwischenzeile.php
+++ einleitung-zwischenzeile.php
@@ -1,11 +1,13 @@
 <?php
 /*
 Plugin Name: Einleitung-Zwischenzeile
-Plugin URI: http://www.perun.net
+Plugin URI: 
 Description: Hiermit wird eine Einleitung, Dachzeile, Zwischenzeile vor dem eigentlichen Artikel ausgegeben.
 Version: 0.1
 Author: Vladimir Simovic
 Author URI: http://www.perun.net
+GitHub Plugin URI: Perun/wp-einleitung-zwischenzeile
+GitHub Plugin URI: https://github.com/Perun/wp-einleitung-zwischenzeile
 */
 
 add_filter( 'the_content', 'einleitung_vor_inhalt' );
